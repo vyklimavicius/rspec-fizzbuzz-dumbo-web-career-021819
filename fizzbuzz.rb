d9c065cc9458e9cfb76e119d990c5d98c06f03dd
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-30 17:25:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require 'pry'
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
@@ -9,5 +10,6 @@ def fizzbuzz(num)
     "Buzz"
   else 
     nil 
+    binding.pry
   end
 end 
